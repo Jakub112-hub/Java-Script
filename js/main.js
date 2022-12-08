59db41127976eee2396a274728d89374c5c1fde6
Kuba <broniszkuba1232@wp.pl>
2022-12-08 21:24:34 +0100

    create new function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 const arrowFunction = () => {
-    
+
 }
+
+
+function test(name){
+    console.log(`mam na imię ${name}`)
+}
+test(`Klaudia`)
+
+
+
+const test2 = (name,age) => console.log(name,age)
+
+test2('Klaudia',23)
+
+
+const days = ['poniedziałek', 'wtorek', 'sroda']
+
+const days2 = days.forEach(day => console.log(day))
